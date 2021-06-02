36113aeb200cdff1bcf178be9f2da6c5497f7899
Emmanuel <flotuz10@gmail.com>
2021-06-02 13:03:07 -0500

    [Fix] Datatable productos y baja de librerias

--- pages/productos/index.js
+++ pages/productos/index.js
@@ -1,104 +1,30 @@
 import Head from "next/head";
 import NavBar from "@common/NavBar";
-import React from 'react';
-import  MDBDataTable  from 'mdbreact';
+import React from "react";
 import { Button, Breadcrumb } from "react-bootstrap";
+import MUIDataTable from "mui-datatables";
+import DefaultLayout from "components/layouts/DefaultLayout";
 
 function Productos() {
-  const DatatablePage = () => {
-    const data = {
-      columns: [
-        {
-          label: 'Name',
-          field: 'name',
-          sort: 'asc',
-          width: 150
-        },
-        {
-          label: 'Position',
-          field: 'position',
-          sort: 'asc',
-          width: 270
-        },
-        {
-          label: 'Office',
-          field: 'office',
-          sort: 'asc',
-          width: 200
-        },
-        {
-          label: 'Age',
-          field: 'age',
-          sort: 'asc',
-          width: 100
-        },
-        {
-          label: 'Start date',
-          field: 'date',
-          sort: 'asc',
-          width: 150
-        },
-        {
-          label: 'Salary',
-          field: 'salary',
-          sort: 'asc',
-          width: 100
-        }
-      ],
-      rows: [
-        {
-          name: 'Tiger Nixon',
-          position: 'System Architect',
-          office: 'Edinburgh',
-          age: '61',
-          date: '2011/04/25',
-          salary: '$320'
-        },
-        {
-          name: 'Garrett Winters',
-          position: 'Accountant',
-          office: 'Tokyo',
-          age: '63',
-          date: '2011/07/25',
-          salary: '$170'
-        },
-        {
-          name: 'Ashton Cox',
-          position: 'Junior Technical Author',
-          office: 'San Francisco',
-          age: '66',
-          date: '2009/01/12',
-          salary: '$86'
-        },
-        {
-          name: 'Donna Snider',
-          position: 'Customer Support',
-          office: 'New York',
-          age: '27',
-          date: '2011/01/25',
-          salary: '$112'
-        }
-      ]
-    };
+  const columns = ["Name", "Company", "City", "State"];
 
-    return (
-      <MDBDataTable
-        striped
-        bordered
-        hover
-        data={data}
-      />
-    );
+  const data = [
+    ["Joe James", "Test Corp", "Yonkers", "NY"],
+    ["John Walsh", "Test Corp", "Hartford", "CT"],
+    ["Bob Herm", "Test Corp", "Tampa", "FL"],
+    ["James Houston", "Test Corp", "Dallas", "TX"],
+  ];
 
-  }
+  const options = {
+    filterType: "checkbox",
+  };
+
   return (
-    <div>
+    <DefaultLayout>
       <Head>
         <title> Productos </title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <NavBar />
 
       <Breadcrumb>
         <Breadcrumb.Item active>Productos</Breadcrumb.Item>
@@ -107,17 +33,15 @@ function Productos() {
       <h1 className="Title" title="Productos">
         Productos
       </h1>
-      <form busqueda>
-        <input type="text" placeholder="Buscar..." />
-        <Button variant="contained" color="default" size="lg">
-          Descargar Excel{" "}
-        </Button>
-      </form>
-      <DatatablePage/>
-    </div>
-    
+
+      <MUIDataTable
+        title={"Employee List"}
+        data={data}
+        columns={columns}
+        options={options}
+      />
+    </DefaultLayout>
   );
-  
 }
 
 export default Productos;
