4a57f7d1d653984a8bcbcaf6b4819bcfda52f7d0
Jaquelyne <jaquelynevazquez1999@gmail.com>
2021-06-04 17:25:28 -0500

    Se modifico, detalles de productos (Tabs)

--- pages/productos/detalles.js
+++ pages/productos/detalles.js
@@ -1,6 +1,5 @@
-
 import DefaultLayout from "components/layouts/DefaultLayout";
-import { 
+import {
     Breadcrumb,
     Button,
     Row,
@@ -9,7 +8,10 @@ import {
     Form,
     Card,
     Image,
- } from "react-bootstrap";
+    Tab,
+    Nav,
+    Tabs,
+} from "react-bootstrap";
 
 function Detalles() {
     return (
@@ -22,147 +24,245 @@ function Detalles() {
             <Container>
                 <Row>
                     <Col>
-                    <Button variant="primary">Guardar</Button> {" "}
-                    <Button variant="outline-danger">Descartar</Button> {" "} {" "}
-                    <Button variant="outline-success">Publicar</Button> 
+                        <Button variant="primary">Guardar</Button> {" "}
+                        <Button variant="outline-danger">Descartar</Button> {" "} {" "}
+                        <Button variant="outline-success">Publicar</Button>
                     </Col>
                 </Row>
 
-                <br/>
+                <br />
 
-            
-            <Row>
-                <Col>
-                    <Card style={{ width: "auto" }}>
-                    <Card.Body>
-                        <Row>
-                        <Col>
-                            <Form>
-                            <Form.Label>Nombre de producto</Form.Label>
-                            <Form.Control
-                                type="text"
-                                placeholder="Nombre de producto"
-                            />
-                            <Form.Label>Nombre de producto en aspel</Form.Label>
-                            <Form.Control
-                                type="text"
-                                placeholder="Nombre de producto en aspel"
-                            />
-                            </Form>
-                        </Col>
-                        <Col>
-                            <Image
-                            src="../headphones.jpg"
-                            alt="Audifonos Unsplash"
-                            thumbnail="true"
-                            height="100px"
-                            width="109px"
-                            rounded
-                            />
-                            <Form.File />
-                        </Col>
-                        </Row>
-                    </Card.Body>
-                    </Card>
-                </Col>
-            </Row>
 
-        </Container>
-
-        <br/>
-
-        <Container>
-            <Row>
-                <Col>
-                    <Card style={{ width: "auto" }}>
-                        <Card.Body>
-                            <Row>
-                                <Col> <div> <Button variant="light">Información general</Button> </div> </Col>
-                                <Col> <div> <Button variant="light">Variantes</Button></div> </Col>
-                                <Col> <div> <Button variant="light">Inventario</Button> </div> </Col>
-                                <Col> <div> <Button variant="light">Publicaciones</Button> </div> </Col>
-                            </Row>
-                             <br/>
-                             <Col>
+                <Row>
+                    <Col>
+                        <Card border="dark" style={{ width: "auto" }}>
+                            <Card.Body>
                                 <Row>
-                                    <div>
-                                        <Form>
-                                            <Form.Label>Text label</Form.Label>
-                                                <select  class="form-control">
-                                                    <option>Producto/Servicio/Kit/Grupo de productos</option>
-                                                    <option>....</option>
-                                                </select>  
-                                        </Form>
-                                    </div>
                                     <Col>
-                                        <div>
-                                            <Form>            
-                                                <Form.Label>Precio de venta</Form.Label>
-                                                <Form.Control
-                                                    type="text"
-                                                    placeholder="$0.00"/>
-                                            </Form>
-                                        </div>
-                                    </Col>
-                                </Row>
-                                <br/>
-                                <Row>
-                                    <div>
                                         <Form>
-                                            <Form.Label>Categorías del producto</Form.Label>
-                                                <select  class="form-control ds-input">
-                                                    <option>Todos/Audio/Audifonos</option>
-                                                    <option>....</option>
-                                                </select>  
-                                        </Form>
-                                    </div>
-                                    <Col>
-                                        <div>
-                                            <Form>            
-                                                <Form.Label>Unidad de medida</Form.Label>
-                                                <Form.Control
-                                                    type="text"
-                                                    placeholder="PZ"/>
-                                            </Form>
-                                        </div>
-                                    </Col>
-                                </Row>
-                                <br/>
-                                <Row>
-                                    <div>
-                                        <Form>            
-                                            <Form.Label>Clave interna</Form.Label>
+                                            <Form.Label>Nombre de producto</Form.Label>
                                             <Form.Control
                                                 type="text"
-                                                placeholder="Sin texto"/>
+                                                placeholder="Nombre de producto"
+                                            />
+                                            <Form.Label>Nombre de producto en aspel</Form.Label>
+                                            <Form.Control
+                                                type="text"
+                                                placeholder="Nombre de producto en aspel"
+                                            />
                                         </Form>
-                                    </div>
+                                    </Col>
                                     <Col>
-                                        <div>
-                                            <Form>            
-                                                <Form.Label>Unidad de medida de compra</Form.Label>
-                                                <Form.Control
-                                                    type="text"
-                                                    placeholder="PZ"/>
-                                            </Form>
-                                        </div>
+                                        <Image
+                                            src="../headphones.jpg"
+                                            alt="Audifonos Unsplash"
+                                            thumbnail="true"
+                                            height="100px"
+                                            width="109px"
+                                            rounded
+                                        />
+                                        <Form.File />
                                     </Col>
                                 </Row>
-                                <Row>
-                                <div class="form-group">
-                                    <label for="exampleFormControlTextarea1">Example textarea</label>
-                                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-                                </div>
-                                </Row>
-                            </Col>
-                        </Card.Body>
-                    </Card>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+
+            </Container>
+
+            <br />
+
+            <Container>
+                <Card border="dark" style={{ width: "auto" }}>
+                    <Card.Body>
+                        <Tabs defaultActiveKey="profile" id="tab">
+                            <Tab eventKey="Tab1" title="Información general">
+                                <br />
+                                <InformacionGeneral />
+                            </Tab>
+                            <Tab eventKey="Tab2" title="Variantes">
+                                <br />
+                                <Variantes />
+                            </Tab>
+                            <Tab eventKey="Tab3" title="Inventario">
+                                <br />
+                                <Inventario />
+                            </Tab>
+                            <Tab eventKey="Tab4" title="Publicaciones">
+                                <br />
+                                <Publicaciones />
+                            </Tab>
+                        </Tabs>
+                    </Card.Body>
+                </Card>
+            </Container>
+
+
+        </DefaultLayout>
+    );
+}
+
+function InformacionGeneral() {
+    return (
+        <Col>
+            <Row>
+                <Col>
+                    <Form>
+                        <Form.Label>Text label</Form.Label>
+                        <select class="form-control ds-input">
+                            <option>Producto/Servicio/Kit/Grupo de productos</option>
+                            <option>....</option>
+                        </select>
+                    </Form>
+                    <Form>
+                        <Form.Label>Categorías del producto</Form.Label>
+                        <select class="form-control ds-input">
+                            <option>Todos/Audio/Audifonos</option>
+                            <option>....</option>
+                        </select>
+                    </Form>
+                    <Form>
+                        <Form.Label>Clave interna</Form.Label>
+                        <Form.Control
+                            type="text"
+                            placeholder="Sin texto" />
+                    </Form>
+                    <div class="form-group">
+                        <label for="exampleFormControlTextarea1">Descripción corta</label>
+                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                    </div>
+                </Col>
+
+                <Col>
+                    <Form>
+                        <Form.Label>Precio de venta</Form.Label>
+                        <Form.Control
+                            type="text"
+                            placeholder="$0.00" />
+                    </Form>
+                    <Form>
+                        <Form.Label>Unidad de medida</Form.Label>
+                        <Form.Control
+                            type="text"
+                            placeholder="PZ" />
+                    </Form>
+                    <Form>
+                        <Form.Label>Unidad de medida de compra</Form.Label>
+                        <Form.Control
+                            type="text"
+                            placeholder="PZ" />
+                    </Form>
+                </Col>
+            </Row>
+        </Col>
+
+    );
+}
+
+function Variantes() {
+    return (
+        <Col>
+            <Row>
+                <Form>
+                    <Form.Label>EJEMPLO VARIANTES</Form.Label>
+                    <Form.Control
+                        type="text"
+                        placeholder="PZ" />
+                </Form>
+            </Row>
+        </Col>
+    );
+}
+
+function Inventario() {
+    return (
+        <Col>
+            <Row>
+                <Col>
+                    <Form.Group controlId="largo">
+                        <Form.Label>Largo(m)</Form.Label>
+                        <Form.Control as="select" size="sm" custom>
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                        </Form.Control>
+                    </Form.Group>
+                </Col>
+                <Col>
+                    <Form.Group controlId="alto">
+                        <Form.Label>Alto(m)</Form.Label>
+                        <Form.Control as="select" size="sm" custom>
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                        </Form.Control>
+                    </Form.Group>
+                </Col>
+                <Col>
+                    <Form>
+                        <Form.Label>Ancho</Form.Label>
+                        <Form.Control
+                            type="text"
+                            placeholder="cm" />
+                    </Form>
                 </Col>
 
             </Row>
-        </Container> 
+        </Col>
+    );
+}
 
-        </DefaultLayout>
+
+function Publicaciones() {
+    return (
+        <Col>
+            <Row>
+                <Col>
+                    <Form>
+                        <Form.Label>Modo de envio</Form.Label>
+                        <select class="form-control ds-input">
+                            <option>No especificado</option>
+                            <option>....</option>
+                        </select>
+                    </Form>
+                </Col>
+                <Col>
+                    <Form>
+                        <Form.Label>Envío gratis</Form.Label>
+                        <select class="form-control ds-input">
+                            <option>Seleccione...</option>
+                            <option>....</option>
+                        </select>
+                    </Form>
+                </Col>
+                <Col>
+                    <Form.Group controlId="alto">
+                        <Form.Label>Cantidad a publicar</Form.Label>
+                        <Form.Control as="select" size="sm" custom placeholder="0">
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                        </Form.Control>
+                    </Form.Group>
+                </Col>
+
+            </Row>
+            <br />
+            <Row>
+                <div>
+                    <h5>
+                        Publicar en:
+                </h5>
+                </div>
+            </Row>
+        </Col>
     );
 }
 
