9a2f3e579d38beb2ab2fb9d925b12e03148112c7
Michelle Patiño <thocokun@gmail.com>
2021-06-04 21:03:24 -0500

    Configuraciones y Usuarios

--- pages/configuraciones/index.js
+++ pages/configuraciones/index.js
@@ -1,34 +1,73 @@
 import DefaultLayout from "components/layouts/DefaultLayout";
 import { Col, Container, Row } from "react-bootstrap";
-
 import styles from "./Index.module.css";
-
+import { Form, Button, Navbar, Alert } from "react-bootstrap";
+import Image from "react-bootstrap/Image";
 function Configuraciones() {
   return (
     <DefaultLayout>
+      <Navbar bg="warning" className="justify-content-center">
+        <Navbar.Brand></Navbar.Brand>
+      </Navbar>
+      <h1>Configuraciones</h1>
       <Container className={styles.lay} fluid="md">
         <Row>
           <Col>
-            <button className={styles.button}>
-                Usuarios
-            </button>
+            <Button className={styles.button} variant="outline-warning">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="100"
+                height="100"
+                fill="currentColor"
+                class="bi bi-person-fill"
+                viewBox="0 0 16 16">
+                <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+              </svg>
+            </Button>{" "}
           </Col>
           <Col>
-            <button className={styles.button}>
-                Usuarios
-            </button>
+            <Button className={styles.button} variant="outline-warning">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="100"
+                height="100"
+                fill="currentColor "
+                class="bi bi-info-circle-fill"
+                viewBox="0 0 16 16"
+              >
+                <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z" />
+              </svg>
+            </Button>{" "}
           </Col>
         </Row>
         <Row>
-        <Col>
-            <button className={styles.button}>
-                Usuarios
-            </button>
+          <Col>
+            <Button className={styles.button} variant="outline-warning">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="100"
+                height="100"
+                fill="currentColor"
+                class="bi bi-bell-fill"
+                viewBox="0 0 16 16"
+              >
+                <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zm.995-14.901a1 1 0 1 0-1.99 0A5.002 5.002 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901z" />
+              </svg>
+            </Button>{" "}
           </Col>
           <Col>
-            <button className={styles.button}>
-                Usuarios
-            </button>
+            <Button className={styles.button} variant="outline-warning">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="100"
+                height="100"
+                fill="currentColor"
+                class="bi bi-key-fill"
+                viewBox="0 0 16 16"
+              >
+                <path d="M3.5 11.5a3.5 3.5 0 1 1 3.163-5H14L15.5 8 14 9.5l-1-1-1 1-1-1-1 1-1-1-1 1H6.663a3.5 3.5 0 0 1-3.163 2zM2.5 9a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
+              </svg>
+            </Button>{" "}
           </Col>
         </Row>
       </Container>
--- pages/configuraciones/usuarios.js
+++ pages/configuraciones/usuarios.js
@@ -1,9 +1,146 @@
+import Table from "react-bootstrap/Table";
+import React from "react";
+import FigureCaption from "react-bootstrap/FigureCaption";
+import DefaultLayout from "components/layouts/DefaultLayout";
+import styles from "./Index.module.css";
+import Button from "react-bootstrap/Button";
+
 function Usuarios() {
-    return (
-        <div>
-            Usuarios
-        </div>
-    );
+  return (
+    <DefaultLayout>
+      <div className={styles.containerUsers}>
+        <StyleTitle />
+        <TableUsuarios />
+      </div>
+    </DefaultLayout>
+  );
+}
+
+function StyleTitle() {
+  return <h1>Bitacora De Usuarios</h1>;
+}
+
+function TableUsuarios() {
+  return (
+    <Table hover>
+      <thead class="thead-dark">
+        <tr>
+          <th>id</th>
+          <th>Nombre</th>
+          <th>Rol</th>
+          <th>Estatus</th>
+          <th>Ultima Conexion</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>Josep</td>
+          <td>Administrador</td>
+          <td>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-circle-fill text-success"
+              viewBox="0 0 16 16"
+            >
+              <circle cx="8" cy="8" r="8" />
+            </svg>
+            Activo
+          </td>
+          <td>DD/MM/YY </td>
+          <td>
+            <Button variant="outline-secondary">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-three-dots-vertical"
+                viewBox="0 0 16 16"
+              >
+                <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
+                <span class="visually-hidden">Button</span>
+              </svg>
+            </Button>{" "}
+          </td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>Jonathan</td>
+          <td>Capturador</td>
+          <td>
+            {" "}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-circle-fill text-success"
+              viewBox="0 0 16 16"
+            >
+              <circle cx="8" cy="8" r="8" />
+            </svg>
+            Activo
+          </td>
+          <td>DD/MM/YY</td>
+          <td>
+            <Button variant="outline-secondary">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-three-dots-vertical"
+                viewBox="0 0 16 16"
+              >
+                <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
+                <span class="visually-hidden">Button</span>
+              </svg>
+            </Button>{" "}
+          </td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td>Joe</td>
+          <td>Capturador</td>
+          <td>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-circle-fill text-secondary"
+              viewBox="0 0 16 16"
+            >
+              <circle cx="8" cy="8" r="8" />
+            </svg>
+            Inactivo
+          </td>
+          <td>DD/MM/YY</td>
+          <td>
+            <Button variant="outline-secondary">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-three-dots-vertical"
+                viewBox="0 0 16 16"
+              >
+                <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
+                <span class="visually-hidden">Button</span>
+              </svg>
+            </Button>{" "}
+          </td>
+          <span class="glyphicon glyphicon-option-vertica"></span>
+        </tr>
+      </tbody>
+    </Table>
+  );
 }
 
 export default Usuarios;
