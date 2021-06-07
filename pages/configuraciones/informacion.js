8b45572b4b4e80c8ef03ce374833f196e2fb4b2d
Michelle Patiño <thocokun@gmail.com>
2021-06-06 23:37:10 -0500

    edit vistas de nav bar en conf, usuario, informacion

--- pages/configuraciones/informacion.js
+++ pages/configuraciones/informacion.js
@@ -2,71 +2,79 @@ import Tabs from "react-bootstrap/Tabs";
 import React from "react";
 import DefaultLayout from "components/layouts/DefaultLayout";
 import styles from "./Index.module.css";
-
+import { Form, Button, Navbar, Alert } from "react-bootstrap";
 
 function Informacion() {
   return (
     <DefaultLayout>
       <div class="container" className={styles.container}>
-        <h1>
-          <svg
+        <Navbar bg="warning" className="justify-content-center">
+          <Navbar.Brand>
+            <h2 class="text-white">
+            <svg
             xmlns="http://www.w3.org/2000/svg"
             width="50"
             height="50"
             fill="currentColor "
-            class="bi bi-info-circle-fill text-warning"
+            class="bi bi-info-circle-fill text-white"
             viewBox="0 0 16 16"
           >
             <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z" />
           </svg>
-          Informacion Del Sistema
-        </h1>
+              Informacion Del Sistema
+            </h2>
+          </Navbar.Brand>
+        </Navbar>
 
         <div class="card">
-
-            <h3>
+          <h3>
             <img src="logos\amazon_icon.png"></img>
-             Amazon</h3>
-            <label for="inputPassword5" class="form-label">
-              
-            </label>
-            <input
-              type="password"
-              id="inputPassword5"
-              class="form-control "
-              placeholder="Api key Amazon"
-              aria-describedby="passwordHelpBlock"
-            ></input>
+            Amazon
+          </h3>
+          <label for="inputPassword5" class="form-label"></label>
+          <input
+            type="password"
+            id="inputPassword5"
+            class="form-control "
+            placeholder="Api key Amazon"
+            aria-describedby="passwordHelpBlock"
+          ></input>
 
-            <h3>
-                <img width="80" src="https://seeklogo.com/images/M/mercado-libre-logo-7848A94FE8-seeklogo.com.png"></img>
-                
-                Mercado Libre</h3>
-            <label for="inputPassword5" class="form-label">
-            </label>
-            <input
-              type="password"
-              id="inputPassword5"
-              class="form-control"
-              placeholder="Api key Mercado Libre"
-              aria-describedby="passwordHelpBlock"
-            ></input>
+          <h3>
+            <img
+              width="80"
+              src="https://seeklogo.com/images/M/mercado-libre-logo-7848A94FE8-seeklogo.com.png"
+            ></img>
+            Mercado Libre
+          </h3>
+          <label for="inputPassword5" class="form-label"></label>
+          <input
+            type="password"
+            id="inputPassword5"
+            class="form-control"
+            placeholder="Api key Mercado Libre"
+            aria-describedby="passwordHelpBlock"
+          ></input>
 
-            <h3>
-            <img width="100" src="https://woocommerce.com/wp-content/themes/woo/images/logo-woocommerce.svg" alt="WooCommerce"></img>
-            <br/>WooComerce</h3>
-            <label for="inputPassword5" class="form-label">
-            </label>
-            <input
-              type="password"
-              id="inputPassword5"
-              class="form-control"
-              placeholder="Api key WooCommerce"
-              aria-describedby="passwordHelpBlock"
-            ></input>
-          </div>
+          <h3>
+            <img
+              width="100"
+              src="https://woocommerce.com/wp-content/themes/woo/images/logo-woocommerce.svg"
+              alt="WooCommerce"
+            ></img>
+            <br />
+            WooComerce
+          </h3>
+          <label for="inputPassword5" class="form-label"></label>
+          <input
+            type="password"
+            id="inputPassword5"
+            class="form-control"
+            placeholder="Api key WooCommerce"
+            aria-describedby="passwordHelpBlock"
+          ></input>
         </div>
-
+      </div>
     </DefaultLayout>
   );
 }
--- pages/configuraciones/usuarios.js
+++ pages/configuraciones/usuarios.js
@@ -3,20 +3,34 @@ import React from "react";
 import DefaultLayout from "components/layouts/DefaultLayout";
 import styles from "./Index.module.css";
 import Button from "react-bootstrap/Button";
+import { Form, Navbar, Alert } from "react-bootstrap";
 
 function Usuarios() {
   return (
     <DefaultLayout>
-      <div className={styles.containerUsers}>
-        <StyleTitle />
+      <Navbar bg="warning" className="justify-content-center">
+          <Navbar.Brand>
+            <h2 class="text-white">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="80"
+                height="80"
+                fill="currentColor"
+                class="bi bi-person-fill text-white"
+                viewBox="0 0 16 16"
+              >
+                <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+              </svg>
+              Usuarios
+            </h2>
+          </Navbar.Brand>
+        </Navbar>
+      <div className={styles.container}>
+        
         <TableUsuarios />
       </div>
     </DefaultLayout>
   );
-}
-
-function StyleTitle() {
-  return <h1>Bitacora De Usuarios</h1>;
 }
 
 function TableUsuarios() {
