0ccf4f059943a731bbf8e7d89dfc988fcabab21b
Emmanuel <flotuz10@gmail.com>
2021-06-08 15:25:56 -0500

    [Fix] Rompí y arreglé esta wea

--- pages/pedidos/index.js
+++ pages/pedidos/index.js
@@ -3,19 +3,17 @@ const { Container, Breadcrumb } = require("react-bootstrap");
 import MUIDataTable from "mui-datatables";
 
 function Pedidos() {
-<<<<<<< HEAD
-    return (
-        <div>
-            
-        </div>
-    );
-=======
   const columns = ["Nombre de Producto", "Fecha", "Cantidad", "Vendido en "];
 
   const data = [
     ["Microfono Shure 550L", "13/01/2021", "2", "WooCommerce"],
     ["Proyector Benq MW550", "20/12/2020", "1", "Mercado Libre"],
-    ["Teatro 5.0 Wharfeadale  Crystal Cr4.3", "19/12/2020", "3", "Mercado Libre"],
+    [
+      "Teatro 5.0 Wharfeadale  Crystal Cr4.3",
+      "19/12/2020",
+      "3",
+      "Mercado Libre",
+    ],
   ];
   const options = {
     filterType: "checkbox",
@@ -24,15 +22,13 @@ function Pedidos() {
         noMatch: "Lo siento,no se encontro",
         toolTip: "Sort",
       },
-    }
+    },
   };
   return (
-    <DefaultLayout>
-        <Breadcrumb>
-            <Breadcrumb.Item active>
-                PEDIDOS 
-            </Breadcrumb.Item>
-        </Breadcrumb>
+    <>
+      <Breadcrumb>
+        <Breadcrumb.Item active>PEDIDOS</Breadcrumb.Item>
+      </Breadcrumb>
       <Container>
         <MUIDataTable
           title={"Lista de Pedidos"}
@@ -41,9 +37,8 @@ function Pedidos() {
           options={options}
         />
       </Container>
-    </DefaultLayout>
+    </>
   );
->>>>>>> main
 }
 
 export default Pedidos;
