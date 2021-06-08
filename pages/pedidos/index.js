bd0e6b6303843dd40cf1f213d44ade227e1c7e54
Joe Miguel <miguerdz1023@gmail.com>
2021-06-08 13:42:00 -0500

    Pedidos[Llenado]

--- pages/pedidos/index.js
+++ pages/pedidos/index.js
@@ -1,11 +1,41 @@
 const { default: DefaultLayout } = require("components/layouts/DefaultLayout");
+const { Container, Breadcrumb } = require("react-bootstrap");
+import MUIDataTable from "mui-datatables";
 
 function Pedidos() {
-    return (
-        <div>
-            <DefaultLayout/>
-        </div>
-    );
+  const columns = ["Nombre de Producto", "Fecha", "Cantidad", "Vendido en "];
+
+  const data = [
+    ["Microfono Shure 550L", "13/01/2021", "2", "WooCommerce"],
+    ["Proyector Benq MW550", "20/12/2020", "1", "Mercado Libre"],
+    ["Teatro 5.0 Wharfeadale  Crystal Cr4.3", "19/12/2020", "3", "Mercado Libre"],
+  ];
+  const options = {
+    filterType: "checkbox",
+    textLabels: {
+      body: {
+        noMatch: "Lo siento,no se encontro",
+        toolTip: "Sort",
+      },
+    }
+  };
+  return (
+    <DefaultLayout>
+        <Breadcrumb>
+            <Breadcrumb.Item active>
+                PEDIDOS 
+            </Breadcrumb.Item>
+        </Breadcrumb>
+      <Container>
+        <MUIDataTable
+          title={"Lista de Pedidos"}
+          data={data}
+          columns={columns}
+          options={options}
+        />
+      </Container>
+    </DefaultLayout>
+  );
 }
 
 export default Pedidos;
