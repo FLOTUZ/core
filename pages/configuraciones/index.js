0ef3b9d5ac29868fd06912a78d2543f9bde9acee
Michelle Patiño <thocokun@gmail.com>
2021-06-09 16:51:57 -0500

    css y botones con direccion

--- pages/configuraciones/index.js
+++ pages/configuraciones/index.js
@@ -2,60 +2,69 @@ import DefaultLayout from "components/layouts/DefaultLayout";
 import { Col, Container, Row } from "react-bootstrap";
 import styles from "./Index.module.css";
 import { Form, Button, Navbar, Alert } from "react-bootstrap";
-import Image from "react-bootstrap/Image";
+import Link from "next/link";
+import { Breadcrumb } from "react-bootstrap";
 
 function Configuraciones() {
   return (
-    <>
-      <Navbar bg="warning" className="justify-content-center">
-        <Navbar.Brand>
-          <h2 className="text-white">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="50"
-              height="50"
-              fill="currentColor"
-              className="bi bi-gear-fill text-white"
-              viewBox="0 0 16 16"
-            >
-              <path d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872l-.1-.34zM8 10.93a2.929 2.929 0 1 1 0-5.86 2.929 2.929 0 0 1 0 5.858z" />
-            </svg>
-            Configuraciones
-          </h2>
-        </Navbar.Brand>
-      </Navbar>
-
-      <Container className={styles.lay} fluid="md">
+  
+    <div className={styles.container}>
+      <h1>Configuraciones</h1>
+      <Breadcrumb>
+        <Breadcrumb.Item active>
+          {" "}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="30"
+            height="30"
+            fill="currentColor"
+            className="bi bi-gear-fill "
+            viewBox="0 0 16 16"
+          >
+            <path d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872l-.1-.34zM8 10.93a2.929 2.929 0 1 1 0-5.86 2.929 2.929 0 0 1 0 5.858z" />
+          </svg>
+          Configuraciones
+        </Breadcrumb.Item>
+      </Breadcrumb>
+      <Container className={styles.container} fluid="md">
         <Row>
           <Col>
-            <Button className={styles.button} variant="outline-warning">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="100"
-                height="100"
-                fill="currentColor"
-                className="bi bi-person-fill"
-                viewBox="0 0 16 16"
-              >
-                <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
-              </svg>
-              <h3>Usuarios</h3>
-            </Button>{" "}
+            <Link href="/configuraciones/usuarios">
+              <a>
+                <Button className={styles.button} variant="outline-warning">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="100"
+                    height="100"
+                    fill="currentColor"
+                    className="bi bi-person-fill"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+                  </svg>
+                  <h3>Usuarios</h3>
+                </Button>{" "}
+              </a>
+            </Link>
           </Col>
           <Col>
-            <Button className={styles.button} variant="outline-warning">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="100"
-                height="100"
-                fill="currentColor "
-                className="bi bi-info-circle-fill"
-                viewBox="0 0 16 16"
-              >
-                <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z" />
-              </svg>
-              <h3>Informacion del sistema</h3>
-            </Button>{" "}
+            <Link href="/configuraciones/informacion">
+              <a>
+                <Button className={styles.button} variant="outline-warning">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="100"
+                    height="100"
+                    fill="currentColor "
+                    className="bi bi-info-circle-fill"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z" />
+                  </svg>
+                  <h3>Informacion del sistema</h3>
+                </Button>{" "}
+              </a>
+            </Link>
           </Col>
         </Row>
         <Row>
@@ -91,7 +100,7 @@ function Configuraciones() {
           </Col>
         </Row>
       </Container>
-    </>
+    </div>
   );
 }
 
--- pages/configuraciones/usuarios.js
+++ pages/configuraciones/usuarios.js
@@ -1,159 +1,137 @@
 import Table from "react-bootstrap/Table";
 import React from "react";
-import DefaultLayout from "components/layouts/DefaultLayout";
 import styles from "./Index.module.css";
 import Button from "react-bootstrap/Button";
-import { Form, Navbar, Alert } from "react-bootstrap";
+import { Breadcrumb } from "react-bootstrap";
 
 function Usuarios() {
   return (
-    <DefaultLayout>
-      <Navbar bg="warning" className="justify-content-center">
-          <Navbar.Brand>
-            <h2 class="text-white">
+    <div className={styles.container}>
+      <h1>Usuarios</h1>
+        <Breadcrumb>
+                <Breadcrumb.Item href="/configuraciones">Configuraciones</Breadcrumb.Item>
+                <Breadcrumb.Item active>Bitacora de Usuarios</Breadcrumb.Item>
+            </Breadcrumb>
+
+      <Table hover>
+        <thead class="thead-dark">
+          <tr>
+            <th>id</th>
+            <th>Nombre</th>
+            <th>Rol</th>
+            <th>Estatus</th>
+            <th>Ultima Conexion</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Josep</td>
+            <td>Administrador</td>
+            <td>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                width="80"
-                height="80"
+                width="16"
+                height="16"
                 fill="currentColor"
-                class="bi bi-person-fill text-white"
+                class="bi bi-circle-fill text-success"
                 viewBox="0 0 16 16"
               >
-                <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+                <circle cx="8" cy="8" r="8" />
               </svg>
-              Usuarios
-            </h2>
-          </Navbar.Brand>
-        </Navbar>
-      <div className={styles.container}>
-        
-        <TableUsuarios />
-      </div>
-    </DefaultLayout>
+              Activo
+            </td>
+            <td>DD/MM/YY </td>
+            <td>
+              <Button variant="outline-secondary">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-three-dots-vertical"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
+                  <span class="visually-hidden">Button</span>
+                </svg>
+              </Button>{" "}
+            </td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Jonathan</td>
+            <td>Capturador</td>
+            <td>
+              {" "}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-circle-fill text-success"
+                viewBox="0 0 16 16"
+              >
+                <circle cx="8" cy="8" r="8" />
+              </svg>
+              Activo
+            </td>
+            <td>DD/MM/YY</td>
+            <td>
+              <Button variant="outline-secondary">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-three-dots-vertical"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
+                  <span class="visually-hidden">Button</span>
+                </svg>
+              </Button>{" "}
+            </td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>Joe</td>
+            <td>Capturador</td>
+            <td>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-circle-fill text-secondary"
+                viewBox="0 0 16 16"
+              >
+                <circle cx="8" cy="8" r="8" />
+              </svg>
+              Inactivo
+            </td>
+            <td>DD/MM/YY</td>
+            <td>
+              <Button variant="outline-secondary">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-three-dots-vertical"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
+                  <span class="visually-hidden">Button</span>
+                </svg>
+              </Button>{" "}
+            </td>
+            <span class="glyphicon glyphicon-option-vertica"></span>
+          </tr>
+        </tbody>
+      </Table>
+    </div>
   );
 }
-
-function TableUsuarios() {
-  return (
-    <Table hover>
-      <thead class="thead-dark">
-        <tr>
-          <th>id</th>
-          <th>Nombre</th>
-          <th>Rol</th>
-          <th>Estatus</th>
-          <th>Ultima Conexion</th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>1</td>
-          <td>Josep</td>
-          <td>Administrador</td>
-          <td>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              class="bi bi-circle-fill text-success"
-              viewBox="0 0 16 16"
-            >
-              <circle cx="8" cy="8" r="8" />
-            </svg>
-            Activo
-          </td>
-          <td>DD/MM/YY </td>
-          <td>
-            <Button variant="outline-secondary">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                class="bi bi-three-dots-vertical"
-                viewBox="0 0 16 16"
-              >
-                <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
-                <span class="visually-hidden">Button</span>
-              </svg>
-            </Button>{" "}
-          </td>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>Jonathan</td>
-          <td>Capturador</td>
-          <td>
-            {" "}
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              class="bi bi-circle-fill text-success"
-              viewBox="0 0 16 16"
-            >
-              <circle cx="8" cy="8" r="8" />
-            </svg>
-            Activo
-          </td>
-          <td>DD/MM/YY</td>
-          <td>
-            <Button variant="outline-secondary">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                class="bi bi-three-dots-vertical"
-                viewBox="0 0 16 16"
-              >
-                <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
-                <span class="visually-hidden">Button</span>
-              </svg>
-            </Button>{" "}
-          </td>
-        </tr>
-        <tr>
-          <td>3</td>
-          <td>Joe</td>
-          <td>Capturador</td>
-          <td>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              class="bi bi-circle-fill text-secondary"
-              viewBox="0 0 16 16"
-            >
-              <circle cx="8" cy="8" r="8" />
-            </svg>
-            Inactivo
-          </td>
-          <td>DD/MM/YY</td>
-          <td>
-            <Button variant="outline-secondary">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                class="bi bi-three-dots-vertical"
-                viewBox="0 0 16 16"
-              >
-                <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
-                <span class="visually-hidden">Button</span>
-              </svg>
-            </Button>{" "}
-          </td>
-          <span class="glyphicon glyphicon-option-vertica"></span>
-        </tr>
-      </tbody>
-    </Table>
-  );
-}
-
 export default Usuarios;
