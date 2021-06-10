2ba6bb9503c6a6457f499b9d6b2735f3242f2e3d
Emmanuel <flotuz10@gmail.com>
2021-06-10 12:51:29 -0500

    Modificacion del datatable de productos

--- pages/productos/index.js
+++ pages/productos/index.js
@@ -1,18 +1,24 @@
 import Head from "next/head";
-import NavBar from "@common/NavBar";
 import React from "react";
 import { Button, Breadcrumb } from "react-bootstrap";
 import MUIDataTable from "mui-datatables";
-import DefaultLayout from "components/layouts/DefaultLayout";
 
 function Productos() {
-  const columns = ["Name", "Company", "City", "State"];
+  const columns = ["Nombre", "Precio", "En Inventario", "Publicado en"];
 
   const data = [
-    ["Joe James", "Test Corp", "Yonkers", "NY"],
-    ["John Walsh", "Test Corp", "Hartford", "CT"],
-    ["Bob Herm", "Test Corp", "Tampa", "FL"],
-    ["James Houston", "Test Corp", "Dallas", "TX"],
+    ["Par De Bocinas Yamaha NS-WA150 Blancas", "$550", "2", "Todos"],
+    ["Proyector Benq MW550", "$10,550", "10", "Mercado Libre"],
+    ["Teatro 5.0 Wharfedale Crystal Cr4.3", "$2300", "4", "Mercado Libre, Amazon"],
+    ["Micrófono Shure 550L", "$3100", "20", "Todos"],
+    ["Par De Bocinas Yamaha NS-WA150 Blancas", "$550", "31", "Todos"],
+    ["Proyector Benq MW550", "$10,550", "12", "Mercado Libre"],
+    ["Teatro 5.0 Wharfedale Crystal Cr4.3", "$2300", "2", "Mercado Libre, Amazon"],
+    ["Micrófono Shure 550L", "$3100", "1", "Todos"],
+    ["Par De Bocinas Yamaha NS-WA150 Blancas", "$550", "5", "Todos"],
+    ["Proyector Benq MW550", "$10,550", "2", "Mercado Libre"],
+    ["Teatro 5.0 Wharfedale Crystal Cr4.3", "$2300", "1", "Mercado Libre, Amazon"],
+    ["Micrófono Shure 550L", "$3100", "4", "Todos"],
   ];
 
   const options = {
@@ -35,7 +41,7 @@ function Productos() {
       </h1>
 
       <MUIDataTable
-        title={"Employee List"}
+        title={""}
         data={data}
         columns={columns}
         options={options}
